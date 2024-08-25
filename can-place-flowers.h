f295286512c89da01568659b4fa0678c9e3b4831
Mustafa Ahsan <optaycal@gmail.com>
2024-08-25 23:52:30 +0500

    Started can-place-flowers

---
+++ can-place-flowers.h
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n)
+    {
+        bool answer = false;
+
+        return answer;
+    }
+};

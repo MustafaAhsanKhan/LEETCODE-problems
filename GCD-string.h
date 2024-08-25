c4b702778ea4ca4606f1d154e4a322cb189d80ab
MustafaAhsanKhan <mustafaahsankhan2006@gmail.com>
2024-08-25 23:36:29 +0500

    Solved GCD-string

---
+++ GCD-string.h
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int gcd(int a, int b)
+{
+    while (b != 0)
+    {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2)
+    {
+        // Check if they have non-zero GCD string.
+        if (str1 + str2 != str2 + str1) {
+            return "";
+        }
+
+        // Get the GCD of the two lengths.
+        int gcdLength = gcd(str1.size(), str2.size());
+        return str1.substr(0, gcdLength);
+    }
+};
--- spiral-matrix.h
+++ spiral-matrix.h
@@ -41,3 +41,22 @@ public:
         return result;
     }
 };
+
+// #include "spiral-matrix.h"
+
+// int main()
+// {
+//     vector<vector<int>> VEC = {{1,2,3},{4,5,6},{7,8,9}};
+
+//     vector<int> result;
+
+//     Solution foo;
+
+//     result = foo.spiralOrder(VEC);
+
+//     for (int i = 0; i < result.size(); ++i)
+//     {
+//         cout << result[i] << " ";
+//     }
+
+// }

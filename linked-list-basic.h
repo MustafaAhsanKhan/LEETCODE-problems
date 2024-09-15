c4430ca1ba75f36fb49a2c7e4ff1de4b38bb6fb8
MustafaAhsanKhan <mustafaahsankhan2006@gmail.com>
2024-09-15 23:54:20 +0500

    Created linked list class

--- linked-list-basic.h
+++ linked-list-basic.h
@@ -5,4 +5,25 @@ struct Node
 {
     int data;
     Node* next;
-}
+};
+
+class LinkedList
+{
+    private:
+    Node* head;
+    int size;
+
+    public:
+    LinkedList()
+    {
+        head = NULL;
+        size = 0;
+    }
+
+    bool isEmpty()
+    {
+        return head == NULL;
+    }
+
+    
+};

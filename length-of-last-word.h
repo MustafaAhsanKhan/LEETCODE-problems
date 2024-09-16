728840187920c3c267ea15148b2d071952404955
MustafaAhsanKhan <mustafaahsankhan2006@gmail.com>
2024-09-16 23:16:55 +0500

    started length of last word

---
+++ length-of-last-word.h
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s)
+    {
+        bool f1 = false;
+        bool f1 = false;
+
+        int size = 0;
+
+        for(int i = s.length(); i >= 0; --i)
+        {
+            if(s[i] >= 'A' && s[i] <= 'a')
+            {
+                f1 = true;
+            }
+            if()
+        }
+    }
+};

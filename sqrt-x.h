79934f6429cbb4d9b8a2d6ec8f9ac26d7c10dcbb
MustafaAhsanKhan <mustafaahsankhan2006@gmail.com>
2024-09-20 21:08:16 +0500

    Formatting

--- sqrt-x.h
+++ sqrt-x.h
@@ -4,10 +4,13 @@ public:
     int mySqrt(int x)
     {
         if (x <= 1)
+        {
             return x;
+        }
         long i = 1;
         long sq = i * i;
-        while (x >= sq) {
+        while (x >= sq)
+        {
             i++;
             sq = i * i;
         }

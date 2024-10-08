9062713a66bce5c505e206e8c6f6d885930420e4
MustafaAhsanKhan <mustafaahsankhan2006@gmail.com>
2024-10-08 22:48:17 +0500

    started basic calculator

---
+++ basic-calculator-2.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int calculate(string s)
+    {
+        stack<int> s1;
+
+        for(int i = 0; i < s.length(); ++i)
+        {
+            
+        }
+    }
+};

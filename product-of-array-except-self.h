8f649ece5356bca01d4f0db5f57baa92082cccc1
MustafaAhsanKhan <mustafaahsankhan2006@gmail.com>
2024-10-14 23:55:19 +0500

    Prefix postfix sum

--- product-of-array-except-self.h
+++ product-of-array-except-self.h
@@ -6,11 +6,12 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums)
     {
+        
         int n = nums.size();
         vector<int> result(n);
 
         int prefix = 1;
-
+        // Prefix sum
         for(int i = 0; i < n; ++i)
         {
             result[i] = prefix;
@@ -18,7 +19,7 @@ public:
         }
 
         int postfix = 1;
-
+        // Postfix sum
         for(int i = n - 1; i >= 0; --i)
         {
             result[i] = result[i] * postfix;

a54fde71a8ede4363b53e0267b13b6a047cb4c83
MustafaAhsanKhan <mustafaahsankhan2006@gmail.com>
2024-10-17 23:25:41 +0500

    Solved array pair sum

---
+++ array-pair-sum.h
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums)
+    {
+        sort(nums.begin(), nums.end());
+
+        int sum = 0;
+
+        for(int i = 0; i < nums.size(); i = i + 2)
+        {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+};

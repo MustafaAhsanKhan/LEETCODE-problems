b4d61411904026d9d50fcf28a64f035a5b4870f6
MustafaAhsanKhan <mustafaahsankhan2006@gmail.com>
2024-10-17 23:18:39 +0500

    Trying relative ranks

---
+++ relative-ranks.h
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <queue>
+
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score)
+    {
+        vector<string> result;
+        priority_queue<int> max_heap(score.begin(), score.end());
+
+    }
+};

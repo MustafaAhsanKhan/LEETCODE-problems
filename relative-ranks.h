83732da49912e4e72f5d7bc2d195811ca93f2eaa
MustafaAhsanKhan <mustafaahsankhan2006@gmail.com>
2024-10-23 23:44:54 +0500

    Solved relative ranks

--- relative-ranks.h
+++ relative-ranks.h
@@ -7,8 +7,28 @@ class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score)
     {
-        vector<string> result;
-        priority_queue<int> max_heap(score.begin(), score.end());
+        int size = score.size();
+        vector<pair<int,int>> v(size);
 
+        for(int i = 0; i < size; ++i)
+        {
+            v[i].first = score[i];
+            v[i].second = i;
+        }
+
+        sort(v.rbegin(), v.rend());
+
+        vector<string> ans(size);
+
+        if(size >= 1) ans[v[0].second] = "Gold Medal";
+        if(size >= 2) ans[v[1].second] = "Silver Medal";
+        if(size >= 3) ans[v[2].second] = "Bronze Medal";
+
+        for(int i = 3; i < size; ++i)
+        {
+            ans[v[i].second] = to_string(i + 1);
+        }
+
+        return ans;
     }
 };

cfaef8f5131af809c1770f9af15a5f586da9cfe7
Mustafa Ahsan <optaycal@gmail.com>
2024-11-07 09:20:23 +0500

    Solved Students unable to eat lunch

--- students-unable-to-eat-lunch.h
+++ students-unable-to-eat-lunch.h
@@ -1,7 +1,8 @@
 #include <iostream>
-using namespace std;
 #include <vector>
 #include <queue>
+using namespace std;
+
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches)
@@ -12,23 +13,26 @@ public:
             q1.push(students[i]);
         }
 
-        bool flag = true;
-        int i = 0;
-        while(flag)
+        int i = 0;  // Sandwich index
+        int rotations = 0;  // Counter for unsuccessful attempts
+        
+        while(!q1.empty() && rotations < q1.size())  // Continue until no progress
         {
-            if(q1.front() == students[i])
+            if(q1.front() == sandwiches[i])  // Student takes sandwich
             {
-                q1.pop();  // Removing that student
-                i++;  // Going to next sandwitch
+                q1.pop();  // Remove student from queue
+                i++;  // Move to the next sandwich
+                rotations = 0;  // Reset unsuccessful attempts counter
             }
-            else  // Sending student to back of queue
+            else  // Student goes to the back of the queue
             {
                 int temp = q1.front();
                 q1.pop();
                 q1.push(temp);
+                rotations++;  // Increment unsuccessful attempts
             }
         }
 
-        return q1.size();
+        return q1.size();  // Remaining students unable to eat
     }
 };
